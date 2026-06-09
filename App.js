02d0e294cdb38a38667871b09d39d048d36e0e61
TOMAS VINICIO LOPEZ <49189962@ort>
2026-06-09 15:37:26 -0300

    lopezNoPiensa

--- App.js
+++ App.js
@@ -1,11 +1,67 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import Logeado from './Logeado';
+import ERROR from './ERROR';
+
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogged, setIsLogged] = useState('false');
+
+
+
+  const manejarEnvio = () => {
+    if (email.trim() === '' || password.trim() === '') {
+      setIsLogged('ERROR');
+    }
+    else if (email == 'simon@galaxies.dev' && password == '*******') {
+      setIsLogged('true');
+    }
+    else {
+      setIsLogged('ERROR');
+    }
+  };
+
+  if (isLogged === 'true') {
+    return <Logeado onCerrarSesion={() => setIsLogged('false')} />;
+  }
+  else if (isLogged === 'ERROR') {
+    return <ERROR xd={() => setIsLogged('false')} />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}//Un estylo declarado abajo
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <Image
+          source={require('./assets/Coquita.png')}
+          style={styles.imagen}></Image>
+
+        <TextInput
+          placeholder="simon@galaxies.dev"
+          placeholderTextColor="#888"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          backgroundColor="#ffffff"
+          style={styles.whigga}
+        />
+        <TextInput
+          placeholder="*******"
+          placeholderTextColor="#888"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          backgroundColor="#ffffff"
+          style={styles.whigga}
+        />
+        <TouchableOpacity style={styles.boton} onPress={manejarEnvio}>
+          <Text style={styles.textoBoton}>Enviar Datos</Text>
+        </TouchableOpacity>
+        <Text style={styles.textoCentrado}>Open up App.js to start working on y!</Text>
+        <StatusBar style="auto" />
+
+      </SafeAreaView>
     </View>
   );
 }
@@ -13,8 +69,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#bebebe',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  whigga: {
+    backgroundColor: '#ffffff',
+    marginVertical: 5,
+    padding: 10,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#2300eb94',
+  },
+  textoCentrado: {
+    textAlign: 'center',
+    fontSize: 16,
+    marginVertical: 40,
+  },
+  imagen: {
+    width: 800,
+    height: 200,
+    resizeMode: 'contain',
+    marginVertical: 60,
+    backgroundColor: '#ffffff',
+  },
+  boton: {
+    backgroundColor: '#2300eb94',
+    marginVertical: 5,
+    padding: 10,
+    borderRadius: 15,
+    width: 200,
+    borderWidth: 1,
+    textColor: '#ffffff',
+  },
+  textoBoton: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#ffffff',
+  },
+
 });
---
+++ ERROR.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+
+export default function ERROR({ xd }) {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.safeArea}>
+        <Text style={styles.bienvenida}>¡ERROR!</Text>
+        
+        <TouchableOpacity style={styles.botonVolver} onPress={xd}>
+          <Text style={styles.textoBoton}>Volver atrás</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#bebebe',
+  },
+  safeArea: {
+    alignItems: 'center',
+  },
+  bienvenida: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#2300eb',
+    marginBottom: 30,
+  },
+  botonVolver: {
+    backgroundColor: '#ffffff',
+    padding: 12,
+    borderRadius: 15,
+    width: 150,
+  },
+  textoBoton: {
+    textAlign: 'center',
+    color: '#000000',
+    fontWeight: 'bold',
+  }
+});
